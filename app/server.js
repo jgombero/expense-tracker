2bf4574e4b3ab2d355a14cfc69bf23be502e680d
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-11 17:42:55 -0700

    Connect server to mongodb cluster

--- app/server.js
+++ app/server.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const { connection } = mongoose;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+});
 
 app.use(cors());
 app.use(express.json());
