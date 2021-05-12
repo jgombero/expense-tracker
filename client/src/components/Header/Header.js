3c4468c55f4e179d4d568ef639884bce2cc019e3
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-12 10:46:34 -0700

    Create reusable header component

---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Jumbotron, Container } from "react-bootstrap";
+
+const Header = ({ title, subtitle }) => {
+  return (
+    <Jumbotron>
+      <Container>
+        <header>
+          <h1 className="title">{title}</h1>
+          <h5 className="subtitle">{subtitle}</h5>
+        </header>
+      </Container>
+    </Jumbotron>
+  );
+};
+
+export default Header;
