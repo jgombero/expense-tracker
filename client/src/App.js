301b34b4d9f6aca7db9859784edf5fcf7dff1bb0
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-12 11:56:42 -0700

    Render ExpenseData component

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavigationBar from "./components/Navbar/Navbar";
 import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
+import ExpenseData from "./components/ExpenseData/ExpenseData";
 
 function App() {
   return (
     <Router>
       <NavigationBar />
       <Route path="/" exact component={ExpenseForm} />
-      <Route path="/expenses" exact />
+      <Route path="/expenses" exact component={ExpenseData} />
     </Router>
   );
 }
