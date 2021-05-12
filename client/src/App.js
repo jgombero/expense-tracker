ff7992dc5c8fc732c13697c3b9aa4de412000083
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-12 11:21:24 -0700

    Render Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import NavigationBar from "./components/Navbar/Navbar";
 import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
 
 function App() {
   return (
     <Router>
+      <NavigationBar />
       <Route path="/" exact component={ExpenseForm} />
       <Route path="/expenses" exact />
     </Router>
