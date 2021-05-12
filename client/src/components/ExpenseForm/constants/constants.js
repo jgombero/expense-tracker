9747f302e1801c997fd7d1cf27b39bcc1cabf87c
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-05-12 10:07:18 -0700

    Add default state and category constants

---
+++ client/src/components/ExpenseForm/constants/constants.js
@@ -0,0 +1,24 @@
+export const defaultState = {
+  name: "",
+  cost: 0,
+  category: "",
+};
+
+export const categories = [
+  "Clothing",
+  "Debt",
+  "Education",
+  "Entertainment",
+  "Food",
+  "Gifts/Donations",
+  "Housing",
+  "Household Items/Supplies",
+  "Insurance",
+  "Medical/Healthcare",
+  "Personal",
+  "Retirement",
+  "Savings",
+  "Transportation",
+  "Utilities",
+  "Other",
+];
